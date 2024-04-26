8baa605951c1fe8d06d36139a86d38c70fe1c587
Pranja1045 <143744000+Pranja1045@users.noreply.github.com>
2024-04-26 14:05:22 +0530

    Add files via upload

--- trainner.py
+++ trainner.py
@@ -3,9 +3,9 @@ import numpy as np
 from PIL import Image
 import os
 # Path for face image database
-path = 'D:/python_programming/pythonPrograms/Face_data'
+path = 'D:/python_programming/Karen/Face_data/'
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-detector = cv2.CascadeClassifier("D:/python_programming/pythonPrograms/F743B93JLQV1868.xml");
+detector = cv2.CascadeClassifier("D:/python_programming/Karen/F743B93JLQV1868.xml");
 # function to get the images and label data
 def getImagesAndLabels(path):
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
@@ -14,7 +14,7 @@ def getImagesAndLabels(path):
     for imagePath in imagePaths:
         PIL_img = Image.open(imagePath).convert('L') # grayscale
         img_numpy = np.array(PIL_img,'uint8')
-        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        id = int(os.path.split(imagePath)[1].split(".")[0])
         faces = detector.detectMultiScale(img_numpy)
         for (x,y,w,h) in faces:
             faceSamples.append(img_numpy[y:y+h,x:x+w])
@@ -22,8 +22,10 @@ def getImagesAndLabels(path):
     return faceSamples,ids
 print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
 faces,ids = getImagesAndLabels(path)
+
+
 recognizer.train(faces, np.array(ids))
-# Save the model into trainer/trainer.yml
-recognizer.write('D:/python_programming/pythonPrograms/trainer.yml') 
+
+
 # Print the numer of faces trained and end program
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
